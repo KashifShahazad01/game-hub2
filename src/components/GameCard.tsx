bb74739609384c9aef2eaca41ad5cb752475aab6
KashifShahzad01 <mkashifshahzad125@gmail.com>
2024-09-12 04:00:26 -0700

    platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Games } from "../hooks/useGames";
+import { PlatformIconList } from "./PlatformIconList";
 
 interface Props {
   game: Games;
@@ -7,11 +8,18 @@ interface Props {
 
 export const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image} />
-      <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
-      </CardBody>
-    </Card>
+    <>
+      <Card borderRadius={10} overflow={"hidden"}>
+        <Image src={game.background_image} />
+        <CardBody>
+          <Heading fontSize={"2xl"}>{game.name}</Heading>
+          <HStack>
+            <PlatformIconList
+              platform={game.parent_platforms.map((p) => p.platform)}
+            />
+          </HStack>
+        </CardBody>
+      </Card>
+    </>
   );
 };
