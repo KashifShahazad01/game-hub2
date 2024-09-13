b425369097917111943abaec3d6a33b4fea93f6c
KashifShahzad01 <mkashifshahzad125@gmail.com>
2024-09-12 20:31:53 -0700

    optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Games } from "../hooks/useGames";
 import { PlatformIconList } from "./PlatformIconList";
 import { CriticScore } from "./CriticScore";
+import { OptimizeImages } from "../services/OptimizeImages";
 
 interface Props {
   game: Games;
@@ -11,7 +12,7 @@ export const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card borderRadius={10} overflow={"hidden"}>
-        <Image src={game.background_image} />
+        <Image src={OptimizeImages(game.background_image)} />
         <CardBody>
           <Heading fontSize={"2xl"}>{game.name}</Heading>
           <HStack justifyContent={"space-between"}>
