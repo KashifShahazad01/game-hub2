10875eaef7d2d8aa8dff059f706cdd87d555dccc
KashifShahzad01 <mkashifshahzad125@gmail.com>
2024-10-06 22:55:48 -0700

    setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./components/theme.ts";
-import App from "./App.tsx";
 import "./index.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
